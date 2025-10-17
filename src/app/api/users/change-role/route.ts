6fb234ad18415253ec33b25ede4cef7c4634ff44
Akshat <akshatjaiswal2518@gmail.com>
2025-10-17 21:00:01 +0530

    add route to change user role

---
+++ src/app/api/users/change-role/route.ts
@@ -0,0 +1,47 @@
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import dbConnect from "@/lib/dbConnect";
+import User from "@/models/User";
+import { NextRequest } from "next/server";
+
+export async function PUT(request: NextRequest) {
+  await dbConnect();
+
+  try {
+    const userId = getDataFromToken(request);
+
+    const user = await User.findById(userId).select("-password");
+
+    if (user?.role === "student") {
+      user.role = "instructor";
+    } else if (user?.role === "instructor") {
+      user.role = "student";
+    } else {
+      return Response.json(
+        {
+          success: false,
+          message: "Invalid user role",
+        },
+        { status: 500 }
+      );
+    }
+
+    await user.save();
+
+    return Response.json(
+      {
+        success: true,
+        message: "User role changed",
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error occured while changing role");
+    return Response.json(
+      {
+        success: false,
+        message: "An error occured while updating role",
+      },
+      { status: 500 }
+    );
+  }
+}
