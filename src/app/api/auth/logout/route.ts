5de125f55790625c541dfa435ba9b28eb335594f
Akshat <akshatjaiswal2518@gmail.com>
2025-10-17 20:06:57 +0530

    add logout route

---
+++ src/app/api/auth/logout/route.ts
@@ -0,0 +1,29 @@
+import dbConnect from "@/lib/dbConnect";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  await dbConnect();
+
+  try {
+    const response = NextResponse.json({
+      success: true,
+      message: "Logout successful",
+    });
+
+    response.cookies.set("token", "", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+    });
+
+    return response;
+  } catch (error) {
+    return Response.json(
+      {
+        success: false,
+        message: "Error occured during logout",
+      },
+      { status: 500 }
+    );
+  }
+}
