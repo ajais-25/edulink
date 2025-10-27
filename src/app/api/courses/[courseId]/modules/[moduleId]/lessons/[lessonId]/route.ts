b49ca1f1b91f5018ddeeebe14a639787a7e79d6d
Akshat <akshatjaiswal2518@gmail.com>
2025-10-27 20:54:36 +0530

    add route to delete lesson

--- src/app/api/courses/[courseId]/modules/[moduleId]/lessons/[lessonId]/route.ts
+++ src/app/api/courses/[courseId]/modules/[moduleId]/lessons/[lessonId]/route.ts
@@ -1,9 +1,12 @@
 import { getDataFromToken } from "@/helpers/getDataFromToken";
+import { deleteFile } from "@/helpers/imagekit/deleteFile";
 import dbConnect from "@/lib/dbConnect";
 import Course from "@/models/Course";
 import Lesson from "@/models/Lesson";
 import Module from "@/models/Module";
+import Quiz from "@/models/Quiz";
 import User from "@/models/User";
+import Video from "@/models/Video";
 import { NextRequest } from "next/server";
 
 export async function GET(
@@ -82,6 +85,111 @@ export async function GET(
       {
         success: false,
         message: "Error occured while fetching lesson",
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(
+  request: NextRequest,
+  {
+    params,
+  }: { params: { courseId: string; moduleId: string; lessonId: string } }
+) {
+  await dbConnect();
+
+  try {
+    const userId = getDataFromToken(request);
+
+    const user = await User.findById(userId).select("-password");
+
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "Unauthorized user",
+        },
+        { status: 401 }
+      );
+    }
+
+    const { courseId, moduleId, lessonId } = params;
+
+    const course = await Course.findById(courseId);
+
+    if (!course) {
+      return Response.json(
+        {
+          success: false,
+          message: "Course not found",
+        },
+        { status: 404 }
+      );
+    }
+
+    const courseModule = await Module.findById(moduleId);
+
+    if (!courseModule) {
+      return Response.json(
+        {
+          success: false,
+          message: "Module not found",
+        },
+        { status: 404 }
+      );
+    }
+
+    const lesson = await Lesson.findById(lessonId);
+
+    if (!lesson) {
+      return Response.json(
+        {
+          success: false,
+          message: "Lesson not found",
+        },
+        { status: 404 }
+      );
+    }
+
+    if (lesson.type === "video") {
+      const lessonVideo = await Video.findById(lesson.videoId);
+
+      if (!lessonVideo) {
+        return Response.json(
+          {
+            success: false,
+            message: "Lesson not found",
+          },
+          { status: 404 }
+        );
+      }
+
+      await Video.findByIdAndDelete(lesson.videoId);
+      const deletedLesson = await Lesson.findByIdAndDelete(lessonId);
+
+      console.log(deletedLesson);
+
+      await deleteFile(lessonVideo.fileId);
+    } else if (lesson.type === "quiz") {
+      const deletedQuiz = await Quiz.findByIdAndDelete(lesson.quizId);
+
+      console.log(deletedQuiz);
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "Lesson deleted successfully",
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error occured while deleting lesson");
+    return Response.json(
+      {
+        success: false,
+        message: "Error occured while deleting lesson",
       },
       { status: 500 }
     );
